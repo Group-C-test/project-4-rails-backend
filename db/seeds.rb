9203289fc06ce95ab13db8bc1f8ec83a7bc8877e
“Alyssa <agoody44@gmail.com>
2021-01-06 18:45:52 -0500

    Adds more seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ Artist.create([
     { name: 'Limp Bizkit', img_url: 'https://imgur.com/btQ3xBe' },
     { name: 'System Of A Down', img_url: 'https://imgur.com/rQoqrqU' },
     { name: 'Evanescence', img_url: 'https://imgur.com/ecdLY3z' },
+    { name: 'Greenday', img_url: 'https://imgur.com/iN0xUv0' }
 ])
 
 Song.create([
@@ -18,4 +19,5 @@ Song.create([
     { name: 'Break Stuff', quote: 'It’s Just One of those Days ', artist_id: 2 },
     { name: 'Chop Suey!', quote: 'Wake up! ', artist_id: 3 },
     { name: 'Wake Me Up Inside!', quote: 'Wake Me Up!', artist_id: 4 },
+    { name: 'Wake Me Up When September Ends', quote: 'Wake Me Up ...', artist_id: 5 }
 ])
